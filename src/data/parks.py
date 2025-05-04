ca98969f7320249f31157b3bbb1b22da8f49806f
ScrappyNozicka <vojtech2012@gmail.com>
2025-05-04 17:53:09 +0100

    park info setup

--- src/data/parks.py
+++ src/data/parks.py
@@ -1,3 +1,12 @@
 parks = [
-    {"park_name": "Test Park", "year_opened": 0000, "annual_attendance": 0},
+    {"park_name": "Forest Frontiers", "year_opened": 1999, "annual_attendance": 250},
+    {"park_name": "Dynamite Dunes", "year_opened": 2000, "annual_attendance": 650},
+    {"park_name": "Leafy Lake", "year_opened": 2003, "annual_attendance": 500},
+    {"park_name": "Diamond Heights", "year_opened": 2006, "annual_attendance": 700},
+    {"park_name": "Evergreen Gardens", "year_opened": 2009, "annual_attendance": 1000},
+    {"park_name": "Bumbly Beach", "year_opened": 2013, "annual_attendance": 750},
+    {"park_name": "Trinity Islands", "year_opened": 2015, "annual_attendance": 750},
+    {"park_name": "Katie's Dreamland", "year_opened": 2018, "annual_attendance": 800},
+    {"park_name": "Pokey Park", "year_opened": 2021, "annual_attendance": 400},
+    {"park_name": "White Water Park", "year_opened": 2023, "annual_attendance": 900},
 ]
