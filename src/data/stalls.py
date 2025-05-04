a30e5701dcb2ccaffc9dfb808cd9a85d6533b9f5
ScrappyNozicka <vojtech2012@gmail.com>
2025-05-04 18:40:53 +0100

    final pre-setup of files

--- src/data/stalls.py
+++ src/data/stalls.py
@@ -2,36 +2,37 @@ stalls = [
     {
         "stall_name": "Fruity Ices Stall",
         "park_name": "Test Park",
-        "foods_served": ["Test Food"],
+        "foods_served": ["Ice Cream"],
     },
     {
         "stall_name": "Chip Shop",
         "park_name": "Test Park",
-        "foods_served": ["Test Food"],
+        "foods_served": ["Chips"],
     },
     {
         "stall_name": "Pizza Stall",
         "park_name": "Test Park",
-        "foods_served": ["Test Food"],
+        "foods_served": ["Pizza"],
     },
     {
         "stall_name": "Burger Bar",
         "park_name": "Test Park",
-        "foods_served": ["Test Food"],
+        "foods_served": ["Burgers"],
     },
     {
         "stall_name": "Drinks Stall",
         "park_name": "Test Park",
-        "foods_served": ["Test Food"],
+        "foods_served": ["Drinks"],
     },
     {
         "stall_name": "Candyfloss Stall",
         "park_name": "Test Park",
-        "foods_served": ["Test Food"],
+        "foods_served": ["Candyfloss"],
     },
     {
         "stall_name": "Popcorn Stall",
         "park_name": "Test Park",
-        "foods_served": ["Test Food"],
-    },
+        "foods_served": ["Popcorn"],
+    }
 ]
+
