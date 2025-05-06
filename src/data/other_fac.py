46c9f63be4efed91f936972c41d6d4f6e5761fda
ScrappyNozicka <vojtech2012@gmail.com>
2025-05-06 19:57:16 +0100

    complete other facilities doc setup

--- src/data/other_fac.py
+++ src/data/other_fac.py
@@ -1,1 +1,11 @@
-other_fac = [{"fac_name": "Toilets", "park_name": "Test Park"}]
+other_fac = [
+    {"fac_name": "Woodland Toilets", "park_name": "Forest Frontiers"},
+    {"fac_name": "Sandy Toilets", "park_name": "Dynamite Dunes"},
+    {"fac_name": "Naval Toilets", "park_name": "Leafy Lake"},
+    {"fac_name": "Sky Toilets", "park_name": "Diamond Heights"},
+    {"fac_name": "Lush Toilets", "park_name": "Evergreen Gardens"},
+    {"fac_name": "Rocky Toilets", "park_name": "Bumbly Beach"},
+    {"fac_name": "Sleepy Toilets", "park_name": "Katie's Dreamland"},
+    {"fac_name": "Rapid Toilets", "park_name": "White Water Park"},
+    {"fac_name": "Innapropriate Toilets", "park_name": "Pokey Park"}
+    ]
