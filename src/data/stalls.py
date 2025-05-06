7a6b8ee0757b64f609824353fab889ab447ab3ea
ScrappyNozicka <vojtech2012@gmail.com>
2025-05-06 20:04:11 +0100

    complete stalls data

--- src/data/stalls.py
+++ src/data/stalls.py
@@ -1,37 +1,222 @@
 stalls = [
     {
         "stall_name": "Fruity Ices Stall",
-        "park_name": "Test Park",
+        "park_name": "Forest Frontiers",
         "foods_served": ["Ice Cream"],
     },
     {
-        "stall_name": "Chip Shop",
-        "park_name": "Test Park",
-        "foods_served": ["Chips"],
-    },
-    {
-        "stall_name": "Pizza Stall",
-        "park_name": "Test Park",
-        "foods_served": ["Pizza"],
-    },
-    {
         "stall_name": "Burger Bar",
-        "park_name": "Test Park",
+        "park_name": "Forest Frontiers",
         "foods_served": ["Burgers"],
     },
     {
         "stall_name": "Drinks Stall",
-        "park_name": "Test Park",
+        "park_name": "Forest Frontiers",
         "foods_served": ["Drinks"],
     },
     {
         "stall_name": "Candyfloss Stall",
-        "park_name": "Test Park",
+        "park_name": "Forest Frontiers",
+        "foods_served": ["Candyfloss"],
+    },
+    {
+        "stall_name": "Fruity Ices Stall",
+        "park_name": "Dynamite Dunes",
+        "foods_served": ["Ice Cream"],
+    },
+    {
+        "stall_name": "Pizza Stall",
+        "park_name": "Dynamite Dunes",
+        "foods_served": ["Pizza"],
+    },
+    {
+        "stall_name": "Burger Bar",
+        "park_name": "Dynamite Dunes",
+        "foods_served": ["Burgers"],
+    },
+    {
+        "stall_name": "Drinks Stall",
+        "park_name": "Dynamite Dunes",
+        "foods_served": ["Drinks"],
+    },
+    {
+        "stall_name": "Fruity Ices Stall",
+        "park_name": "Leafy Lake",
+        "foods_served": ["Ice Cream"],
+    },
+    {
+        "stall_name": "Chip Shop",
+        "park_name": "Leafy Lake",
+        "foods_served": ["Chips"],
+    },
+    {
+        "stall_name": "Candyfloss Stall",
+        "park_name": "Leafy Lake",
         "foods_served": ["Candyfloss"],
     },
     {
         "stall_name": "Popcorn Stall",
-        "park_name": "Test Park",
+        "park_name": "Leafy Lake",
+        "foods_served": ["Popcorn"],
+    },
+    {
+        "stall_name": "Chip Shop",
+        "park_name": "Diamond Heights",
+        "foods_served": ["Chips"],
+    },
+    {
+        "stall_name": "Pizza Stall",
+        "park_name": "Diamond Heights",
+        "foods_served": ["Pizza"],
+    },
+    {
+        "stall_name": "Burger Bar",
+        "park_name": "Diamond Heights",
+        "foods_served": ["Burgers"],
+    },
+    {
+        "stall_name": "Drinks Stall",
+        "park_name": "Diamond Heights",
+        "foods_served": ["Drinks"],
+    },
+    {
+        "stall_name": "Candyfloss Stall",
+        "park_name": "Diamond Heights",
+        "foods_served": ["Candyfloss"],
+    },
+    {
+        "stall_name": "Fruity Ices Stall",
+        "park_name": "Evergreen Gardens",
+        "foods_served": ["Ice Cream"],
+    },
+    {
+        "stall_name": "Chip Shop",
+        "park_name": "Evergreen Gardens",
+        "foods_served": ["Chips"],
+    },
+    {
+        "stall_name": "Pizza Stall",
+        "park_name": "Evergreen Gardens",
+        "foods_served": ["Pizza"],
+    },
+    {
+        "stall_name": "Burger Bar",
+        "park_name": "Evergreen Gardens",
+        "foods_served": ["Burgers"],
+    },
+    {
+        "stall_name": "Drinks Stall",
+        "park_name": "Evergreen Gardens",
+        "foods_served": ["Drinks"],
+    },
+    {
+        "stall_name": "Candyfloss Stall",
+        "park_name": "Evergreen Gardens",
+        "foods_served": ["Candyfloss"],
+    },
+    {
+        "stall_name": "Popcorn Stall",
+        "park_name": "Evergreen Gardens",
+        "foods_served": ["Popcorn"],
+    },
+    {
+        "stall_name": "Fruity Ices Stall",
+        "park_name": "Bumbly Beach",
+        "foods_served": ["Ice Cream"],
+    },
+    {
+        "stall_name": "Pizza Stall",
+        "park_name": "Bumbly Beach",
+        "foods_served": ["Pizza"],
+    },
+    {
+        "stall_name": "Drinks Stall",
+        "park_name": "Bumbly Beach",
+        "foods_served": ["Drinks"],
+    },
+    {
+        "stall_name": "Chip Shop",
+        "park_name": "Trinity Islands",
+        "foods_served": ["Chips"],
+    },
+    {
+        "stall_name": "Burger Bar",
+        "park_name": "Trinity Islands",
+        "foods_served": ["Burgers"],
+    },
+    {
+        "stall_name": "Drinks Stall",
+        "park_name": "Trinity Islands",
+        "foods_served": ["Drinks"],
+    },
+    {
+        "stall_name": "Candyfloss Stall",
+        "park_name": "Trinity Islands",
+        "foods_served": ["Candyfloss"],
+    },
+    {
+        "stall_name": "Fruity Ices Stall",
+        "park_name": "Katie's Dreamland",
+        "foods_served": ["Ice Cream"],
+    },
+    {
+        "stall_name": "Drinks Stall",
+        "park_name": "Katie's Dreamland",
+        "foods_served": ["Drinks"],
+    },
+    {
+        "stall_name": "Candyfloss Stall",
+        "park_name": "Katie's Dreamland",
+        "foods_served": ["Candyfloss"],
+    },
+    {
+        "stall_name": "Popcorn Stall",
+        "park_name": "Katie's Dreamland",
+        "foods_served": ["Popcorn"],
+    },
+    {
+        "stall_name": "Pizza Stall",
+        "park_name": "Pokey Park",
+        "foods_served": ["Pizza"],
+    },
+    {
+        "stall_name": "Burger Bar",
+        "park_name": "Pokey Park",
+        "foods_served": ["Burgers"],
+    },
+    {
+        "stall_name": "Drinks Stall",
+        "park_name": "Pokey Park",
+        "foods_served": ["Drinks"],
+    },
+    {
+        "stall_name": "Popcorn Stall",
+        "park_name": "Pokey Park",
+        "foods_served": ["Popcorn"],
+    },
+    {
+        "stall_name": "Fruity Ices Stall",
+        "park_name": "White Water Park",
+        "foods_served": ["Ice Cream"],
+    },
+    {
+        "stall_name": "Chip Shop",
+        "park_name": "White Water Park",
+        "foods_served": ["Chips"],
+    },
+    {
+        "stall_name": "Drinks Stall",
+        "park_name": "White Water Park",
+        "foods_served": ["Drinks"],
+    },
+    {
+        "stall_name": "Candyfloss Stall",
+        "park_name": "White Water Park",
+        "foods_served": ["Candyfloss"],
+    },
+    {
+        "stall_name": "Popcorn Stall",
+        "park_name": "White Water Park",
         "foods_served": ["Popcorn"],
     }
 ]
